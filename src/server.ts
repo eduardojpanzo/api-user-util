a85de7d13ba9971fe000406eca2dc749275ba8f9
João Eduardo Panzo <joaoeduardopanzo@gmail.com>
2023-06-05 05:21:27 +0100

    feat: create and conect a database

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,15 @@
-console.log("Hello");
+import { prisma } from "./lib/prisma";
+
+async function main() {
+  // ... you will write your Prisma Client queries here
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
